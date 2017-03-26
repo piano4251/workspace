f5d84d9a483d9e776813edd63af01aebaca652e3
YG <YG@YG-PC>
2017-03-26 15:34:46 +0900

    static 변경

--- DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/settings.py
@@ -131,3 +131,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ("/work/git/workspace/DjangoWeb/elections/static/", )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
